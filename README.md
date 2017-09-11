7eaada3f4fc6c99d6b3398d6be2b714b5dd5c446
James Vickery <dev@jamesvickery.net>
2017-09-11 00:11:39 +0000

    Added test script
    Also added some basic instructions to the readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # RFID Sandbox
 
-## Trying out RFID stuff with an RFID-RC522
+Trying out RFID stuff with an RFID-RC522
+
+<hr>
+
+## Set Up
+
+1. `sudo raspi-config`, enable SPI
+2. `sudo pip install spidev`, `RPi.GPIO`
+3. `sudo pip install `pi-rc522`
+
+## Test
+
+1. `python test.py`
+2. Wave tags over reader
+
---
+++ test.py
@@ -0,0 +1,31 @@
+from pirc522 import RFID
+rdr = RFID()
+
+
+def main():
+  while True:
+    rdr.wait_for_tag()
+    (error, tag_type) = rdr.request()
+    if not error:
+      print("Tag detected")
+      (error, uid) = rdr.anticoll()
+      if not error:
+        print("UID: " + str(uid))
+        # Select Tag is required before Auth
+        if not rdr.select_tag(uid):
+          # Auth for block 10 (block 2 of sector 2) using default shipping key A
+          if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
+            # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+            print("Reading block 10: " + str(rdr.read(10)))
+            # Always stop crypto1 when done working
+            rdr.stop_crypto()
+
+
+if __name__ == "__main__":
+  try:
+    main()
+  except:
+    print "\n\naborting...\n"
+
+rdr.cleanup()
+
